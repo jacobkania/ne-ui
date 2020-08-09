eed918a81242161f12ef576bc87b61b4bde39566
Jacob Kania <25391879+jacobkania@users.noreply.github.com>
2020-08-09 15:55:40 -0700

    moved version to dist

--- dist/Button/Button.js
+++ dist/Button/Button.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import classNames from 'classnames';
+import {createUseStyles} from 'react-jss';
+import BaseColorContext from 'ne-ui/utils/BaseColorContext';
+import colorTransform, { targets } from 'ne-ui/utils/colorTransform';
+import PropTypes from 'prop-types';
 
-const Button = ({label, onClick}) => {
-    return <button onClick={onClick}>{label}</button>;
+const useStyles = createUseStyles({
+    button: (props) => ({
+        borderWidth: "0",
+        borderRadius: "0.5em",
+        padding: "0.5em 1em",
+        fontSize: "1em",
+        boxShadow: `0.25rem 0.25rem 0.5rem ${colorTransform(props.baseColor.primary, targets.shadow)}, -0.25rem -0.25rem 0.5rem ${colorTransform(props.baseColor.primary, targets.highlight)}`,
+        color: props.baseColor.text,
+
+        "&:active": {
+            boxShadow: `inset 0.25rem 0.25rem 0.5rem ${colorTransform(props.baseColor.primary, targets.shadow)}, inset -0.25rem -0.25rem 0.5rem ${colorTransform(props.baseColor.primary, targets.highlight)}`,
+            color: props.baseColor.text,
+            "& span": {
+                fontSize: "0.95em"
+            }
+        }
+    }),
+    flat: (props) => ({
+        background: props.baseColor.primary,
+    }),
+    inward: (props) => ({
+        background: `linear-gradient(145deg, ${colorTransform(props.baseColor.primary, targets.darkerAccent)}, ${colorTransform(props.baseColor.primary, targets.lighterAccent)})`,
+    }),
+    outward: (props) => ({
+        background: `linear-gradient(145deg, ${colorTransform(props.baseColor.primary, targets.lighterAccent)}, ${colorTransform(props.baseColor.primary, targets.darkerAccent)})`,
+    }),
+});
+
+const Button = ({ label, onClick, type }) => {
+    const baseColor = useContext(BaseColorContext);
+    const classes = useStyles({ baseColor });
+
+    return <button className={classNames(classes.button, classes[type])} onClick={onClick}><span>{label}</span></button>;
+};
+
+Button.propTypes = {
+    label: PropTypes.string.isRequired,
+    onClick: PropTypes.func,
+    type: PropTypes.oneOf(['flat', 'inward', 'outward'])
+}
+
+Button.defaultProps = {
+    type: 'flat'
 }
 
 export default Button;
---
+++ dist/Button/Button.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Button from './Button';
+
+test('renders label inside button', () => {
+  const { getByText } = render(<Button label="Test label" />);
+  const linkElement = getByText(/Test label/i);
+  expect(linkElement).toBeInTheDocument();
+});
